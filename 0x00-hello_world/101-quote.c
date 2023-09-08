88f4529b0fec47e63bdd271e17635d67b782a8d0
basantmohamed3 <basantm263@gmail.com>
2023-09-07 19:44:23 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-const char *message = "and that piece of art is useful\\\" - Dora Korpar,
-2015-10-19\\n";
+const char *message = "and that piece of art is useful
+\" - Dora Korpar, 2015-10-19\n", 59);
 write(STDERR_FILENO, message, 59);
 return (1);
 }
