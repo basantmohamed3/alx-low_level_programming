67ab6fba026459ef213545b3f14e9b67c9338558
basantmohamed3 <basantm263@gmail.com>
2023-09-07 19:38:52 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <unistd.h>
 
 /**
-* main - Entry point
-* Return: Always 1 (Success)
-*/
+ * main - Entry point
+ * Return: Always 1 (Error)
+ */
 int main(void)
 {
-write(STDERR_FILENO, "and that piece of art is useful\"-
-Dora Korpar, 2015-10-19\n", 59);
+const char *message = "and that piece of art is useful\\\" - Dora Korpar,
+2015-10-19\\n";
+write(STDERR_FILENO, message, 59);
 return (1);
 }
+
