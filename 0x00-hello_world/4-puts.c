d8b1fa0c135f910c9f56fd516c8d7164c679802e
basantmohamed3 <basantm263@gmail.com>
2023-09-07 19:12:00 -0700

    task -4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
- /* Entry point of the program */
- puts("\"Programming is like building a multilingual puzzle");    
- /* Indicates successful program execution */
- return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
