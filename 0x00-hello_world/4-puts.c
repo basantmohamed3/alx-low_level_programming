00f34f2478e609f98b0a6fcd2edf95396f518b0d
basantmohamed3 <basantm263@gmail.com>
2023-09-07 19:06:58 -0700

    task -5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0); 
+ /* Entry point of the program */
+ puts("\"Programming is like building a multilingual puzzle");    
+ /* Indicates successful program execution */
+ return 0;
 }
