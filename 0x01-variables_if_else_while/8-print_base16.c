069be98f6f5a317ae9c1eac4ba1926d84967bf83
basantmohamed3 <basantm263@gmail.com>
2023-09-22 00:04:14 -0700

    task- 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function that prints all hexadecimal numbers
+ * in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char hex_digit = '0';
+
+while (hex_digit <= '9')
+{
+putchar(hex_digit);
+hex_digit++;
+}
+
+hex_digit = 'a';
+
+while (hex_digit <= 'f')
+{
+putchar(hex_digit);
+hex_digit++;
+}
+
+putchar('\n');
+
+return (0);
+}
