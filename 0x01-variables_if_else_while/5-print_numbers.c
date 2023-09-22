1f0986e2adb42af3a446dc1066f8295a91a4212e
basantmohamed3 <basantm263@gmail.com>
2023-09-21 23:53:40 -0700

    task- 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function that prints all single-digit
+ * numbers in base 10, starting from 0 and followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+ int number = 0;
+
+while (number <= 9)
+{
+putchar(number + '0');
+number++;
+}
+putchar('\n');
+return (0);
+}
