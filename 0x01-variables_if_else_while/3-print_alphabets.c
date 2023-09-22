40e57f6c85c5aa17bcc6f111d1dcf7939af3b442
basantmohamed3 <basantm263@gmail.com>
2023-09-21 23:46:00 -0700

    task- 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function that prints the alphabet
+ * in lowercase and uppercase, followed by a newline character.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     char lowercase = 'a';
